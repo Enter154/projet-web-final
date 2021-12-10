c671aa7bd3fe4319ca4455d367a8140fbe60d548
leon <leon7502045@gmail.com>
2021-12-10 16:30:53 +0100

    Site avec create update delete et read

---
+++ src/View/User/delete.php
@@ -0,0 +1,13 @@
+<div class="row">
+    <div class="col-12">
+        <form action="index.php?route=utilisateur&action=delete" method="post">
+            <div class="mb-3">
+                <label for="id" class="form-label">ID : </label>
+                <input type="number" name="id" class="form-control" id="id">
+            </div>
+            <div class="col-auto">
+                <button type="submit" class="btn btn-primary mb-3">Send</button>
+            </div>
+        </form>
+    </div>
+</div>
